472d175cf2b6b47a482094c992b6285e6bae0919
afonso miguel <afonso.miguel1995@gmail.com>
2022-02-15 16:49:55 +0000

    mid adding an edit part

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,28 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit]
   def index
     @articles = Article.all
   end
 
   def show; end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+
+    redirect_to articles_path
+  end
+
+  def edit; end
+
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
   def set_article
     @article = Article.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'articles#index'
-  get ':id', to: 'articles#show'
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'article#edit'
 end
